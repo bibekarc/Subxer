f1fdeb365c22fd085912ca5cedc9cd5f83685658
unknown <you@example.com>
2024-08-07 20:48:35 +0545

    redeploy

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -10,11 +10,20 @@ const LeftSidebar = () => {
   const router = useRouter();
   const pathname = usePathname();
 
-  const { userId } = useAuth();
+  const { userId, signOut } = useAuth();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(); // Call Clerk's signOut function
+      router.push("/sign-up"); // Redirect to sign-up page
+    } catch (error) {
+      console.error("Sign out failed:", error);
+    }
+  };
 
   return (
-    <section className='custom-scrollbar leftsidebar'>
-      <div className='flex w-full flex-1 flex-col gap-6 px-6'>
+    <section className="custom-scrollbar leftsidebar">
+      <div className="flex w-full flex-1 flex-col gap-6 px-6">
         {sidebarLinks.map((link) => {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
@@ -35,24 +44,26 @@ const LeftSidebar = () => {
                 height={24}
               />
 
-              <p className='text-light-1 max-lg:hidden'>{link.label}</p>
+              <p className="text-light-1 max-lg:hidden">{link.label}</p>
             </Link>
           );
         })}
       </div>
 
-      <div className='mt-10 px-6'>
+      <div className="mt-10 px-6">
         <SignedIn>
-          <SignOutButton signOutCallback={() => router.push("/sign-in")}>
-            <div className='flex cursor-pointer gap-4 p-4'>
+          <SignOutButton>
+            <div
+              onClick={handleSignOut}
+              className="flex cursor-pointer gap-4 p-4"
+            >
               <Image
-                src='/assets/logout.svg'
-                alt='logout'
+                src="/assets/logout.svg"
+                alt="logout"
                 width={24}
                 height={24}
               />
-
-              <p className='text-light-2 max-lg:hidden'>Logout</p>
+              <p className="text-light-2 max-lg:hidden">Logout</p>
             </div>
           </SignOutButton>
         </SignedIn>
