fbde81aa95242a4451cd4199adc7b195c4c14790
unknown <you@example.com>
2024-08-10 10:28:57 +0545

    reversed the order of posts in profile section

--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -50,9 +50,12 @@ async function ThreadsTab({ currentUserId, accountId, accountType }: Props) {
     redirect("/");
   }
 
+  // Reverse the order of threads
+  const reversedThreads = [...result.threads].reverse();
+
   return (
     <section className='mt-9 flex flex-col gap-10'>
-      {result.threads.map((thread) => (
+      {reversedThreads.map((thread) => (
         <ThreadCard
           key={thread._id}
           id={thread._id}
